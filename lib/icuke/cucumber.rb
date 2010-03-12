a48d424670a6fa24581e0ef249df77338657a047
Rob Holland <rob@the-it-refinery.co.uk>
2010-03-12 17:23:11 +0000

    Try other keyboards if a key is missing. Don't close the simulator application if a scenario fails.

--- lib/icuke/cucumber.rb
+++ lib/icuke/cucumber.rb
@@ -34,9 +34,9 @@ class ICukeWorld
   def record
     Curl::Easy.http_get(BASE_URL + "/record")
   end
-    
+  
   def tap(label, pause = 1)
-    unless frame = page.xpath(%Q{//*[@label="#{label}"]/frame}).first
+    unless frame = page.xpath(%Q{//*[(contains(@traits, "button") or contains(@traits, "updates_frequently") or contains(@traits, "keyboard_key")) and @label="#{label}"]/frame}).first
       raise %Q{No element labelled "#{label}" found in: #{response}}
     end
     
@@ -54,7 +54,18 @@ class ICukeWorld
   def type(textfield, text)
     tap(textfield)
     text.split('').each do |c|
-      tap(c.downcase, 0)
+      tried_next_keyboard = false
+      begin
+        tap(c.downcase, 0)
+      rescue Exception => e
+        begin
+          tap('next keyboard', 0)
+          tried_next_keyboard = true
+          retry
+        rescue
+          raise e
+        end
+      end
     end
     tap('Return')
   end
@@ -99,6 +110,9 @@ Then /^show me the screen$/ do
   puts response
 end
 
-After do
-  quit
+After do |scenario|
+  # Leave the application on-screen to help debug the failure. This is only
+  # useful if this is the last scenario run though, so if you want to make use
+  # of this run the failing scenario on it's own.
+  quit unless scenario.failed?
 end
