7e0add2016b394544066225d81a900cd53a19561
Rob Holland <rob@the-it-refinery.co.uk>
2010-04-22 15:04:59 +0100

    Try and make simulation start-up more reliable. If it's appears to be taking too long, quit out of XCode and retry.

--- lib/icuke/simulator.rb
+++ lib/icuke/simulator.rb
@@ -6,6 +6,7 @@ require 'timeout'
 
 module ICuke
   class Simulator
+    include Timeout
     include HTTParty
     base_uri 'http://localhost:50000'
     
@@ -22,41 +23,67 @@ module ICuke
       simulator = Appscript.app('iPhone Simulator.app')
       simulator.quit if simulator.is_running?
       
-      xcode = Appscript.app('Xcode.app')
-      xcode.launch
-      xcode.open project_file
-      project = xcode.active_project_document.project
-      
-      initial_build_configuration_type = project.active_build_configuration_type.get
-      project.active_build_configuration_type.set project.build_configuration_types[options[:configuration]]
-      
-      if options[:target]
-        initial_target = project.active_target.get
-        project.active_target.set project.targets[options[:target]]
+      begin
+        project = open_project(project_file)
+        
+        settings = {
+          :active_build_configuration_type => project.build_configuration_types[options[:configuration]]
+        }
+        if options[:target]
+          settings[:active_target] = project.targets[options[:target]]
+        end
+        
+        with_settings(project, settings) do
+          executable = project.active_executable.get
+          options[:env].each_pair do |name, value|
+            executable.make :new => :environment_variable,
+                            :with_properties => { :name => name, :value => value, :active => true }
+          end
+          
+          project.launch_
+          
+          timeout(30) do
+            sleep(0.5) until simulator.is_running?
+          end
+        end
+      rescue Timeout::Error
+        xcode = Appscript.app('Xcode.app')
+        xcode.quit if xcode.is_running?
+        sleep(0.5) until !xcode.is_running?
+        retry
       end
       
-      executable = project.active_executable.get
-      options[:env].each_pair do |name, value|
-        executable.make :new => :environment_variable,
-                        :with_properties => { :name => name, :value => value, :active => true }
-      end
-      project.launch_
-      
-      Timeout::timeout(30) do
+      timeout(30) do
         begin
           view
         rescue Errno::ECONNREFUSED
-          sleep(0.1)
+          sleep(0.5)
           retry
         end
       end
-    ensure
-      # Restore the active build settings
-      if initial_build_configuration_type
-        project.active_build_configuration_type.set initial_build_configuration_type
+    end
+    
+    def open_project(project_file)
+      xcode = Appscript.app('Xcode.app')
+      unless xcode.active_project_document.get and xcode.active_project_document.project.path.get == project_file
+        xcode.launch
+        xcode.open project_file
       end
-      if initial_target
-        project.active_target.set initial_target
+      xcode.active_project_document.project
+    end
+    
+    def with_settings(project, settings, &block)
+      initial_settings = {}
+      
+      settings.each_key { |setting| initial_settings[setting] = project.send(setting).get }
+      settings.each_pair do |setting, value|
+        project.send(setting).set value
+      end
+      
+      yield
+    ensure
+      initial_settings.each_pair do |setting, value|
+        project.send(setting).set value
       end
     end
     
